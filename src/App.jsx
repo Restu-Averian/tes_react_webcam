4dbdc4f218809d3c01b1ff0f1f4e44e50ed623a1
Restu-Averian <restuaverianputra123@gmail.com>
2024-02-06 09:26:26 +0700

    coba tes di mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import Webcam from "react-webcam";
-import "./camera.css";
-import { useState } from "react";
 
 function App() {
-  const [open, setOpen] = useState(false);
   return (
     <>
-      <button
-        onClick={() => {
-          try {
-            setOpen(true);
-          } catch (err) {
-            alert(JSON.stringify(err));
-          }
+      <Webcam
+        style={{
+          objectFit: "cover",
+          position: "absolute",
+          inset: 0,
+          width: "100%",
+          height: "100%",
         }}
-      >
-        Open Camera
-      </button>
-      {open && <Webcam />}
+        videoConstraints={{
+          aspectRatio: 1,
+        }}
+      />
     </>
   );
 }
